69e68988e6adaf22e7e2f917e2c0dbade12fa387
gpbl <io@gpbl.dev>
2023-07-19 20:00:12 -0500

    Last state (CSS)

--- website/examples/multiple-months.tsx
+++ website/examples/multiple-months.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { DayPicker } from 'react-day-picker';
 
 export default function App() {
-  return <DayPicker id="example" numberOfMonths={12} />;
+  return <DayPicker id="example" numberOfMonths={12} showWeekNumber />;
 }
